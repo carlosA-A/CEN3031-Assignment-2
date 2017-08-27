d851cb29420bf7dd653cb1ab3945b2adcf74ee2a
carlosA-A <c.avogadro@ufl.edu>
2017-08-27 13:54:08 -0400

    Added map funtionality

--- listingController.js
+++ listingController.js
@@ -3,10 +3,13 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
     $scope.hide = true;
+    $scope.latitude = 0;
+    $scope.longitude = 0;
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
+
     $scope.addListing = function() {
       var newListing = {
         "code": $scope.code,
@@ -23,9 +26,18 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       {
         $scope.latitude = $scope.listings[index].coordinates.latitude;
         $scope.longitude = $scope.listings[index].coordinates.longitude;
-    
+        $scope.initialize();
       }
       $scope.detailedInfo = $scope.listings[index].name;
     };
+    $scope.initialize = function() {
+      var map = new google.maps.Map(document.getElementById('map_div'), {
+         center: {lat: $scope.latitude, lng: $scope.longitude},
+         zoom: 18
+      });
+   }    
+   
+   google.maps.event.addDomListener(window, 'load', $scope.initialize);   
+
   }
 ]);
